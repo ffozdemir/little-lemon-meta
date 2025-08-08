5f3bb8af90c5979685c15de17980f3d88dcc652a
Fetullah Furkan Özdemir <furkanozdemir51@yandex.com.tr>
2025-08-08 14:49:58 +0200

    Refactor Hero component layout for improved responsiveness and styling

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -4,33 +4,33 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className="bg-primary-green p-2">
-      <Container className="flex flex-col md:flex-row items-center justify-center md:justify-around text-center md:text-left p-2">
-        <div className="md:w-1/3">
-          <h1 className="text-4xl font-bold text-primary-yellow font-family-markazi">
-            Little Lemon
-          </h1>
-          <h2 className="text-white text-lg">Chicago</h2>
-          <p className="text-lg text-white mt-4">
-            Experience the best of Mediterranean cuisine with a modern twist.
-            Our dishes are crafted with fresh, locally sourced ingredients to
-            bring you a taste of the Mediterranean right in the heart of
-            Chicago.
-          </p>
-          <button className="mt-6 px-6 py-3 bg-primary-yellow text-secondary-blue rounded-2xl hover:bg-yellow-600 transition-colors cursor-pointer">
-            Reserve a Table
-          </button>
-        </div>
-        <div className=" md:w-1/4 lg:w-1/5 sm:w-1/2  mt-8 md:mt-0">
-          <Image
-            src="/images/restaurantfood.jpg"
-            alt="Restaurant Food"
-            className="rounded-2xl shadow-lg object-cover"
-            width={400}
-            height={300}
-            priority
-            
-          />
+    <section className="relative bg-primary-green py-16">
+      <Container className="relative">
+        <div className="grid md:grid-cols-2 gap-8 items-center">
+          <div className="text-center md:text-left">
+            <h1 className="text-6xl font-bold text-primary-yellow font-family-markazi">
+              Little Lemon
+            </h1>
+            <h2 className="text-3xl text-white mb-4">Chicago</h2>
+            <p className="text-lg text-white mb-8 max-w-md">
+              We are a family owned Mediterranean restaurant, focused on
+              traditional recipes served with a modern twist.
+            </p>
+            <button className="px-8 py-3 bg-primary-yellow text-secondary-blue rounded-2xl hover:bg-yellow-500 transition-colors cursor-pointer font-bold">
+              Reserve a Table
+            </button>
+          </div>
+
+          <div className="relative h-[400px] md:h-[450px] md:absolute md:right-10 md:top-48 md:-translate-y-1/2 md:w-[35%] rounded-2xl overflow-hidden">
+            <Image
+              src="/images/restaurantfood.jpg"
+              alt="Restaurant Food"
+              fill
+              priority
+              className="object-cover rounded-2xl"
+              sizes="(max-width: 768px) 100vw, 45vw"
+            />
+          </div>
         </div>
       </Container>
     </section>
